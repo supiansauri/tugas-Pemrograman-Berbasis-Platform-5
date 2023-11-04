b0d5bf1cf3e4250e3eae53f91da238cd042a51eb
supiansauri <114654547+supiansauri@users.noreply.github.com>
2023-11-04 22:36:50 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const modul1 = require("./modul1");
+const modul2 = require("./modul2");
+const modul3 = require("./modul3");
+const modul4 = require("./modul4");
+
+console.log(modul1.greet());
+console.log("6 + 3 =", modul2.add(6, 3));
+console.log("3 * 3 =", modul3.multiply(3, 3));
+console.log("Square of 6 is", modul4.square(6));
---
+++ modul1.js
@@ -0,0 +1,5 @@
+const greet = () => {
+    return "Perkenalkan Nama Saya Supian Sauri";
+  };
+  
+  module.exports = { greet };
